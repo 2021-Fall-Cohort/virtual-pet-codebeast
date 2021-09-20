7d4da5a0d6f85bbddc94fe865f7a1c503c2de8f1
jgard2019 <jwgardner22@gmail.com>
2021-09-20 19:14:13 -0400

    9-20-2021 current

---
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,130 @@
+package virtual_pet;
+
+public class VirtualPet {
+    private String name;
+    private String maneColor;
+
+    private int age = 10;
+    private int hungerLevel = 10;
+    private int thirstLevel = 20;
+    private int boredom;
+    private int timeTick = 0;
+
+
+    //This is our constructor
+    public VirtualPet(String name, String maneColor) {
+        this.name = name;
+        this.maneColor = maneColor;
+    }
+
+    // General Methods for Pet
+    public void greeting() {
+        System.out.println("Hey my name is " + name
+                + " I am a lion who's age is " + age
+                + " my mane color is " + maneColor);
+    }
+
+    public void petStatus() {
+        if (hungerLevel <= 35) {
+            System.out.println("=^._.^=");
+        } else if (hungerLevel <= 65) {
+            System.out.println(" (⋟_⋞) ");
+        } else if (hungerLevel >= 100) {
+            System.out.println(" X‿X ");
+        }
+        System.out.println("Pet age is " + age);
+        System.out.println("Pet hunger is " + hungerLevel);
+        System.out.println("Pet thirst is " + thirstLevel + "\n");
+    }
+
+    public void feed() {
+        hungerLevel -= 10;
+        boredom += 10;
+        timeTick++;
+        System.out.println("Lion devours a bleeding rib-eye.");
+    }
+
+    public void giveWater() {
+        thirstLevel += 10;
+        hungerLevel += 5;
+        timeTick++;
+        System.out.println("Lion is");
+    }
+
+    public void performTricks() {
+        hungerLevel -= 10;
+        boredom -= 10;
+        thirstLevel -= 10;
+        timeTick++;
+        System.out.println("Pet lion performs tricks!");
+    }
+
+    //for every 2 actions (feed, performTricks) time will increment
+    public void timePass() {
+        timeTick = 0;
+        age += 10;
+        hungerLevel += 1;
+        thirstLevel += 1;
+
+        checkHungerMinMax(hungerLevel);
+        checkThirstMinMax(thirstLevel);
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setManeColor(String maneColor) {
+        this.maneColor = maneColor;
+    }
+
+    private int checkHungerMinMax(int x) {
+        hungerLevel = x;
+
+        if (hungerLevel > 100) {
+            hungerLevel = 100;
+        } else if (hungerLevel < 0) {
+            hungerLevel = 0;
+        }
+        return hungerLevel;
+    }
+
+    private int checkThirstMinMax(int x) {
+        thirstLevel = x;
+
+        if (thirstLevel > 100) {
+            thirstLevel = 100;
+        } else if (thirstLevel < 0) {
+            thirstLevel = 0;
+        }
+        return thirstLevel;
+    }
+
+
+    //Getters
+    public int getAge() {
+        return age;
+    }
+
+    public int getHungerLevel() {
+        return hungerLevel;
+    }
+
+    public int getTime() {
+        return timeTick;
+    }
+
+    public int getThirstLevel() {
+        return thirstLevel;
+    }
+
+    public int getBoredom() {
+        return boredom;
+    }
+
+}
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,75 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
-    }
+        //Instantiate new scanner object and new lion1 object
+        //lion1 starts basically blank, now we can take input to make changes
 
+        Scanner userInput = new Scanner(System.in);
+        VirtualPet lion1 = new VirtualPet(" ", " ");
+
+        //Prompt user for name of pet, and set into Lion class
+        System.out.println("Please name your pet");
+        String nameOfPet = userInput.nextLine();
+        lion1.setName(nameOfPet);
+
+        //Prompt user for color of mane, and set into Lion class
+        System.out.println("Please give your lion a mane color? ");
+        String maneColor = userInput.next();
+        lion1.setManeColor(maneColor);
+
+        lion1.greeting();
+
+        int userChoice = 0;
+        do {
+
+            //Check age, (set higher later) if(true) game over
+            if (lion1.getAge() >= 200 ){
+                System.out.println("Your lion died of old age.");
+                break;
+            }
+            //check hunger, if(true) game over
+            if (lion1.getHungerLevel() >= 200) {
+                System.out.println("Your lion died of starvation.");
+                break;
+            }
+            if (lion1.getThirstLevel() >= 200){
+                System.out.println("Your lion died of thirst");
+                break;
+            }
+
+            //check timeTick, if >= 2, call timePass()
+            //if timePass() is called, it will internally reset timeTick
+            //getTime calls timeTick and evaluates
+            if (lion1.getTime() >= 2) {
+                lion1.timePass();
+            }
+
+            System.out.println("What would you like to do?");
+            System.out.println("Press 1 for pet status");
+            System.out.println("Press 2 to feed pet");
+            System.out.println("Press 3 to give water");
+            System.out.println("Press 4 to perform tricks");
+            System.out.println("Press 5 to exit");
+
+            userChoice = userInput.nextInt();
+            if (userChoice == 1) {
+                lion1.petStatus();
+            } else if (userChoice == 2) {
+                lion1.feed();
+            } else if (userChoice == 3) {
+                lion1.giveWater();
+            } else if (userChoice == 4) {
+                lion1.performTricks();
+            } else if (userChoice == 5){
+                break;
+            }
+        } while (userChoice <= 4);
+
+        System.out.println("Exiting Program");
+        System.exit(0);
+    }
 }
