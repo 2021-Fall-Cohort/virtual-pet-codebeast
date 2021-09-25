af4681f653f2f207cf759fd880988692a5bf9d33
gosman10 <gosman10@hotmail.com>
2021-09-25 19:30:51 -0400

    added tests, might be basic but those are the ideas I could come with

--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -9,25 +9,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class VirtualPetTest {
 
-//
-//    @Test
-//    public void testIfPetHasAName() {
-//        Lion Simba = new Lion("Simba", 9, 15, 30);
-//        String name = Simba.getName();
-//        assertEquals("Simba", name);
-//    }
-//}
-////    @Test
-////    public void testIfCanCreateAPet(){
-////        //ArrayList<Monkey> testArray = new ArrayList<Monkey>();
-////        Monkey myMokey =  new Monkey("Simba",19,20,50));
-////        assertTrue(myMokey != null);
-////
-////    }
-////    @Test
-////    public void testIfPetCanDrink(){
-////        Dog puppy = new Dog("Yuppie",19,25,100);
-////
-////    }
-////}
+
+    @Test
+    public void testIfPetHasAName() {
+        Lion Simba = new Lion("Simba", 9, 15, 30);
+        String name = Simba.getName();
+        assertEquals("Simba", name);
+    }
+
+   @Test
+  public void testIfCanCreateAPet(){
+ArrayList<Monkey> testArray = new ArrayList<Monkey>();
+      Monkey myMonkey =  new Monkey("Simba",19,20,50);
+      assertTrue(myMonkey != null);
+
+}
+    @Test
+    public void testIfPetCanDrink(){
+        Dog puppy = new Dog("Yuppie",19,25,100);
+        puppy.giveWater();
+        assertEquals(80, puppy.getThirstLevel());
+    }
+    @Test
+    public void testIfPetCanBeFed(){
+        Dog puppy = new Dog("Yuppie",19,100,100);
+        puppy.feed();
+        assertEquals(80,puppy.getThirstLevel());
+    }
 }
